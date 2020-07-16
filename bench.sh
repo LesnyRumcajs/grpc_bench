8da617027b745e234b08dc2f140af999eb08c95c
LesnyRumcajs <lesny.rumcajs@gmail.com>
2020-07-16 22:20:29 +0200

    added some optimization to Rust Tonic, increased benchmark requests

--- bench.sh
+++ bench.sh
@@ -4,5 +4,5 @@
 # Rust Tonic benchmark
 docker run --name rust_tonic_test --rm --cpus 1 -p 50051:50051 -d rust_tonic_test
 sleep 5
-ghz --proto=proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure -n 100000 -d "{ \"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051
+ghz --proto=proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure -n 200000 -d "{ \"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051
 docker container stop rust_tonic_test
