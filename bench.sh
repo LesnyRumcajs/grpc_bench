982221e65f4628136d776460e28f3a5f062137dd
LesnyRumcajs <lesny.rumcajs@gmail.com>
2020-07-17 18:19:01 +0200

    added Ruby benchmark

--- bench.sh
+++ bench.sh
@@ -8,4 +8,7 @@
 ./run_single_bench.sh go_grpc_test
 
 # Cpp grpc benchmark
- ./run_single_bench.sh cpp_grpc_test
+./run_single_bench.sh cpp_grpc_test
+
+# Ruby benchmark
+./run_single_bench.sh ruby_grpc_test
