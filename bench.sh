00a441e66a81d9d4e787c8e4432c15f8d16a5627
Trisfald <trisfald@gmail.com>
2020-07-17 09:41:08 +0000

    use a dockerized version of ghz

--- bench.sh
+++ bench.sh
@@ -2,7 +2,7 @@
 
 
 # Rust Tonic benchmark
-docker run --name rust_tonic_test --rm --cpus 1 -p 50051:50051 -d rust_tonic_test
+docker run --name rust_tonic_test --rm --cpus 1 --network=host -d rust_tonic_test
 sleep 5
-ghz --proto=proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure -n 200000 -d "{ \"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051
+docker run --name ghz --rm --network=host -v ${PWD}/proto:/proto:ro --entrypoint=ghz infoblox/ghz:0.0.1 --proto=/proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure -n 200000 -d "{\"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051
 docker container stop rust_tonic_test
