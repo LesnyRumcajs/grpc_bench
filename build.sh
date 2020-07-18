160cf2bfbd01610ef2fc39ce0a245c046465ff0b
LesnyRumcajs <LesnyRumcajs@users.noreply.github.com>
2020-07-18 16:15:59 +0200

    crystal bench

--- build.sh
+++ build.sh
@@ -15,3 +15,5 @@ docker build --force-rm -f scala_akka/Dockerfile -t scala_akka_test .
 docker build --force-rm -f java_grpc/Dockerfile -t java_grpc_test .
 
 docker build --force-rm -f kotlin_grpc/Dockerfile -t kotlin_grpc_test .
+
+docker build --force-rm -f crystal_grpc/Dockerfile -t crystal_grpc_test .
---
+++ crystal_grpc/src/server.cr
@@ -0,0 +1,18 @@
+require "grpc"
+require "grpc/http2"
+
+require "./protobufs/helloworld_services.pb"
+require "./protobufs/helloworld.pb"
+
+class HelloWorldHandler < Helloworld::Greeter
+  def say_hello(request : Helloworld::HelloRequest) : Helloworld::HelloReply
+    Helloworld::HelloReply.new(message: request.name)
+  end
+end
+
+grpc = GRPC::Server.new
+grpc << HelloWorldHandler.new
+
+server = HTTP2::ClearTextServer.new([grpc])
+server.listen "0.0.0.0", 50051
+
