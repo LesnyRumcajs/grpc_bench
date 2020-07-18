4e9be977eb8ab6abc7a0df79ac421af0cdc235de
LesnyRumcajs <LesnyRumcajs@users.noreply.github.com>
2020-07-18 10:46:43 +0200

    Store results, show executive summary at the end

---
+++ analyze/lib/test_result.rb
@@ -0,0 +1,21 @@
+class TestResult
+  attr_accessor :name, :total_time, :ok_responses, :req_per_second, :average_response_time
+
+  def initialize(name, total_time, ok_responses, average_response_time)
+    @name = name.split(/_test.report/).first
+    @total_time = total_time
+    @ok_responses = ok_responses
+    @req_per_second = calculate_req_per_second(ok_responses, total_time)
+    @average_response_time = average_response_time
+  end
+
+  def to_s
+      "#{@name.ljust(15)}: #{@req_per_second}"
+  end
+
+  private
+
+  def calculate_req_per_second(ok_responses, total_time)
+    (ok_responses / total_time).to_i
+  end
+end
---
+++ analyze/results_analyze.rb
@@ -0,0 +1,30 @@
+this_dir = File.expand_path(File.dirname(__FILE__))
+lib_dir = File.join(this_dir, 'lib')
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require 'test_result'
+
+if ARGV.empty?
+  puts 'You need to provide report directory path!'
+  exit 1
+end
+
+report_directory = ARGV[0]
+unless Dir.exists? report_directory
+  puts "Report directory [#{report_directory}] does not exist."
+  exit 1
+end
+
+results = []
+files = Dir.glob("#{report_directory}/*.report")
+files.each do |file|
+  total_time = File.read(file).scan(/Total:\s*((?:\d|\.)+)/)[0][0].to_f
+  ok_responses = File.read(file).scan(/\[OK\]\s*(\d+)/)[0][0].to_f
+  average_response_time = File.read(file).scan(/Average:\s*((?:\d|\.)+)/)[0][0].to_f
+  results << TestResult.new(File.basename(file), total_time, ok_responses, average_response_time)
+end
+
+puts 'Results (successful requests per second)'
+results.sort_by(&:req_per_second).reverse_each do |result|
+  puts result
+end
