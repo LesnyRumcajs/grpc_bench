8749cc1ced8b625b217c50bcfc154c805753d4b5
Trisfald <trisfald@gmail.com>
2020-07-21 12:15:13 +0000

    increase number of connections to 5

--- run_single_bench.sh
+++ run_single_bench.sh
@@ -9,5 +9,5 @@ echo "==> Running benchmark for ${NAME}..."
 mkdir -p "${REPORT_DIR}"
 docker run --name "${NAME}" --rm --cpus 1 --network=host -d -t ${NAME}
 sleep 5
-docker run --name ghz --rm --network=host -v "${PWD}"/proto:/proto:ro --entrypoint=ghz infoblox/ghz:0.0.1 --proto=/proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure --duration "${GRPC_BENCHMARK_DURATION}" -d "{\"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051 > "${REPORT_DIR}"/"${NAME}".report
+docker run --name ghz --rm --network=host -v "${PWD}"/proto:/proto:ro --entrypoint=ghz infoblox/ghz:0.0.1 --proto=/proto/helloworld/helloworld.proto --call=helloworld.Greeter.SayHello --insecure --connections=5 --duration "${GRPC_BENCHMARK_DURATION}" -d "{\"name\":\"it's not as performant as we expected\"}" 127.0.0.1:50051 > "${REPORT_DIR}"/"${NAME}".report
 docker container stop "${NAME}"
