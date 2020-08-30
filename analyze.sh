f924c789f0c055f94ba8dae94fcde1966528bd48
Bruno Borges <bruno.borges@microsoft.com>
2020-08-30 00:15:29 +0000

    save bench configuration parameters to a file in the result folder

--- analyze.sh
+++ analyze.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 RESULTS_DIR=${RESULTS_DIR:-"${@}"}
+
 echo "-----"
 echo "Benchmark finished. Detailed results are located in: ${RESULTS_DIR}"
+
 docker run --name analyzer --rm \
 	-v "${PWD}/analyze:/analyze:ro" \
 	-v "${PWD}/${RESULTS_DIR}:/reports:ro" \
 	ruby:2.7-buster ruby /analyze/results_analyze.rb reports ||
 	exit 1
+
+cat > ${RESULTS_DIR}/bench.params << EOF
+Benchmark Execution Parameters:
+ - GRPC_BENCHMARK_DURATION=${GRPC_BENCHMARK_DURATION}
+ - GRPC_SERVER_CPUS=${GRPC_SERVER_CPUS}
+ - GRPC_SERVER_RAM=${GRPC_SERVER_RAM}
+ - GRPC_CLIENT_CONNECTIONS=${GRPC_CLIENT_CONNECTIONS}
+ - GRPC_CLIENT_CONCURRENCY=${GRPC_CLIENT_CONCURRENCY}
+ - GRPC_CLIENT_QPS=${GRPC_CLIENT_QPS}
+ - GRPC_CLIENT_CPUS=${GRPC_CLIENT_CPUS}
+ - GRPC_REQUEST_PAYLOAD=${GRPC_REQUEST_PAYLOAD}
+EOF
