3cfb850d659f523e950c6537a19ee1a5481ff7c2
Pierre Fenoll <pierrefenoll@gmail.com>
2022-01-09 22:06:03 +0100

    forget about logging, lets keep it minimal
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- erlang_grpcbox_bench/src/egb_handler.erl
+++ erlang_grpcbox_bench/src/egb_handler.erl
@@ -3,26 +3,12 @@
 -module(egb_handler).
 -behavior(helloworld_greeter_bhvr).
 
--include_lib("kernel/include/logger.hrl").
-
 -export([say_hello/2]).
-
--define(NOW(), erlang:monotonic_time()).
--define(SINCE(T), erlang:convert_time_unit(?NOW()-T, native, microsecond)).
 
 %% @doc Unary RPC
 -spec say_hello(ctx:ctx(), helloworld_pb:hello_request()) ->
           {ok, helloworld_pb:hello_reply(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
 
-say_hello(Ctx, Req) ->
-    StartTime = erlang:monotonic_time(),
-    ?LOG_DEBUG(#{status=>handling, req=>Req}),
-
-    #{name := Name
-     } = Req,
-
-    Rep = #{message => Name
-           },
-
-    ?LOG_INFO(#{status=>handled, rep=>Rep, in_us=>?SINCE(StartTime)}),
+say_hello(Ctx, #{name := Name}) ->
+    Rep = #{message => Name},
     {ok, Rep, Ctx}.
