cebedd31f3c342a2254bb124fb7dad0821e487f2
Caio <c410.f3r@gmail.com>
2024-09-19 23:50:08 -0300

    Update wtx

--- rust_wtx_bench/src/main.rs
+++ rust_wtx_bench/src/main.rs
@@ -2,34 +2,33 @@ pub mod grpc_bindings;
 
 use wtx::{
     data_transformation::dnsn::QuickProtobuf,
-    grpc::{GrpcStatusCode, Server, ServerData},
+    grpc::{GrpcManager, GrpcStatusCode},
     http::{
-        server_framework::{post, Router},
-        ReqResBuffer, Request, StatusCode,
+        server_framework::{post, Router, ServerFrameworkBuilder, State},
+        ReqResBuffer, StatusCode,
     },
 };
 
 fn main() -> wtx::Result<()> {
-    let cpus = std::env::var("GRPC_SERVER_CPUS")
-        .ok()
-        .and_then(|var| var.parse().ok())
-        .unwrap_or(1);
     tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(cpus)
+        .worker_threads(std::env::var("GRPC_SERVER_CPUS")?.parse()?)
         .enable_all()
         .build()?
         .block_on(async move {
             let router = Router::paths(wtx::paths!((
                 "/helloworld.Greeter/SayHello",
                 post(say_hello)
-            ),));
-            Server::new(router).listen("0.0.0.0:50051", |_| {}).await
+            )))?;
+            ServerFrameworkBuilder::new(router)
+                .with_req_aux(|| QuickProtobuf::default())
+                .listen("0.0.0.0:50051", |_| {})
+                .await
         })
 }
 
 async fn say_hello(
-    _: &mut Request<ReqResBuffer>,
-    _: ServerData<QuickProtobuf>,
-) -> wtx::Result<(StatusCode, GrpcStatusCode)> {
-    Ok((StatusCode::Ok, GrpcStatusCode::Ok))
+    state: State<'_, (), GrpcManager<QuickProtobuf>, ReqResBuffer>,
+) -> wtx::Result<StatusCode> {
+    *state.ra.status_code_mut() = GrpcStatusCode::Ok;
+    Ok(StatusCode::Ok)
 }
