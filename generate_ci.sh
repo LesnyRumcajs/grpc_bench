4e0dc9de87fd5378de6a02bf44722a2d58e8a43e
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2024-10-29 18:34:05 +0100

    fix generation

--- generate_ci.sh
+++ generate_ci.sh
@@ -48,7 +48,7 @@ jobs:
 
     - name: Get changed files
       id: finder
-      uses: tj-actions/changed-files@v44
+      uses: tj-actions/changed-files@v45
 
     - name: Output whether base changed
       id: base
